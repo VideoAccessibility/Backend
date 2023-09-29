2fc75dc5d938e82b8940d1ff4f563a8ba1fc42f1
Sina Elahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2023-09-29 19:04:45 +0330

    add public or private

--- video/serializer.py
+++ video/serializer.py
@@ -5,3 +5,4 @@ class VideosSerializer(serializers.Serializer):
     created_at = serializers.DateTimeField()
     title = serializers.CharField()
     video_path = serializers.CharField()
+    public_or_private = serializers.CharField()
