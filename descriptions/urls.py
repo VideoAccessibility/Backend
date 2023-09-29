40d8e64ab9ebe6db07b90f816811c69775d0a33d
Sina Elahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2023-09-29 19:14:19 +0330

    Update urls.py

--- descriptions/urls.py
+++ descriptions/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Descriptions.as_view())
+    path('', views.Descriptions.as_view()),
+    path('star/', views.Star.as_view())
 ]
