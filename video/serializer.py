5718f1a22bbcb8931e625f532a8131578df304bd
SinaElahimanesh <sinaaelahimanesh@gmail.com>
2024-05-15 21:09:40 +0330

    fix issue of serializers

--- video/serializer.py
+++ video/serializer.py
@@ -5,4 +5,5 @@ class VideosSerializer(serializers.Serializer):
     created_at = serializers.DateTimeField()
     title = serializers.CharField()
     video_path = serializers.CharField()
+    url = serializers.CharField()
     public_or_private = serializers.CharField()
